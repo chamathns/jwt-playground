66b94f50c71c002a331e1f36aae324b38b2d910b
Chamath Samarawickrama <chamaths@wso2.com>
2023-11-11 11:21:45 +0530

    style the encoded text area

--- src/Encoded.jsx
+++ src/Encoded.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { StyledPaper, StyledTextarea } from './AppStyles';
+import { TextField } from '@mui/material';
+import { StyledPaper } from './AppStyles';
+import { blueGrey } from '@mui/material/colors';
 
 function Encoded({ jwt, handleJwtChange }) {
         return (
                 <StyledPaper elevation={3}>
-                        <StyledTextarea 
-                                minRows={10} 
-                                onChange={handleJwtChange} 
-                                placeholder="Paste a token here..."
+                        <TextField
+                                multiline
+                                minRows={10}
                                 value={jwt}
+                                onChange={handleJwtChange}
+                                placeholder="Paste a token here..."
+                                variant="outlined"
+                                fullWidth
+                                InputProps={{
+                                        style: { color: blueGrey[500], fontFamily: 'Menlo' },
+                                }}
                         />
                 </StyledPaper>
         );
